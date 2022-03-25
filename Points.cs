890e600b9b113afbfcc54378c76d1bac8eb1e70a
Daha-Doom <102369786+Daha-Doom@users.noreply.github.com>
2022-03-26 03:22:38 +0700

    Update Points.cs

--- Points.cs
+++ Points.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +9,41 @@ namespace Map
     internal class Points
     {
         private string name;
-        private int quantity;
+        private int quantity, id;
         private double x, y;
 
-        public Points(string _name, int _quantity, double _x, double _y)
+        public Points(int _id, string _name, int _quantity, double _x, double _y)
         {
+            this.id = _id;
             this.name = _name;
             this.quantity = _quantity;
             this.x = _x;
             this.y = _y;
+        }
+
+        public int GetID()
+        { 
+            return id; 
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+
+        public double GetX()
+        {
+            return x;
+        }
+
+        public double GetY()
+        {
+            return y;
         }
     }
 }
